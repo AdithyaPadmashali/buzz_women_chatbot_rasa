477c30905a65c1a5c02b84258e732b56744f5011
Abhayda007 <abhayda2001@gmail.com>
2022-06-13 17:08:59 +0530

    Final commit

--- actions/actions.py
+++ actions/actions.py
@@ -15,10 +15,14 @@ from rasa_sdk.executor import CollectingDispatcher
 
 import pandas as pd
 import numpy as np
+
 df = pd.read_csv("Corpus.csv")
 
-#
-#
+import json
+
+f = open("Corpus.json")
+corpus = json.load(f)
+
 skill_submitted = []
 class ActionShowIdeas(Action):
 
@@ -29,36 +33,45 @@ class ActionShowIdeas(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        current_idea = tracker.get_slot('idea')
-        if(current_idea):
-            skill_submitted.extend(current_idea)
-        investment_level = tracker.get_slot('investment_level')
+        try:
+            skill_submitted.extend(tracker.get_slot('idea'))
+        except TypeError:
+            investment_level = tracker.get_slot('investment_level')
+            levels = []
+            for content in corpus:
+                if content['investment_level'] == investment_level :
+                    levels.append(content['category'])
+            dispatcher.utter_message(text="You can think about {text}".format(text = ", ".join(levels)))
+            buttons = []
+            buttons.append({'title': 'Tell me more about it!' , 'payload': '/request_for_idea_details'})
+            dispatcher.utter_message(text="press button to continue... or tell me a new idea if you want to talk about it...", buttons=buttons)
+            
+            return[SlotSet("idea", levels)]
+            
 
         print(skill_submitted)
-        if(investment_level):
-            print("investment level = ", investment_level)
-
         ideas = []
+
         for s in skill_submitted:
             print(s)
-            ideas.append(df.loc[df['Idea'] == s,'Idea'].to_list())
+            for content in corpus:
+                if s in list(map(lambda x : x.lower(), content['detailed_tags'])):
+                    ideas.append(content)
         
-        # ideas = ' ,'.join(ideas)
+        options = []
+        options.extend(i['detailed_tags'][:-1] for i in ideas)
+        print(options)
+        
 
-        # print(ideas)
-        if(not skill_submitted and not investment_level):
+        if(not skill_submitted):
             dispatcher.utter_message(text="Sorry, didnt get what you meant")
+            return []
         else:
-            dispatcher.utter_message(text="You can think about {ideas}".format(ideas = ideas))
-        
-            # dispatcher.utter_message(text="But considering your skills, these seem to be a good match: ")
+            dispatcher.utter_message(text="You can think about {ideas}".format(ideas = ', '.join(options[-1])))
+    
         buttons = []
-
         buttons.append({'title': 'Tell me more about it!' , 'payload': '/request_for_idea_details'})
-        # buttons.append({'title': 'No talk about something else' , 'payload': 'payload_value'})
-
         dispatcher.utter_message(text="press button to continue... or tell me a new idea if you want to talk about it...", buttons=buttons)
-        # dispatcher.utter_message(text= 'message you want to display' , buttons=buttons)
 
         return []
 
@@ -74,26 +87,24 @@ class ActionTakeIdeaForward(Action):
 
         idea_submitted = tracker.get_slot('idea')
 
-        dispatcher.utter_message(text = 'Taking your idea of '+ str(idea_submitted)+' to the next level!')
+        ideas = []
 
-        # skill_submitted = tracker.get_slot('skill')
+        for s in idea_submitted:
+            print(s)
+            for content in corpus:
+                if s in list(map(lambda x : x.lower(), content['detailed_tags'])):
+                    ideas.append(content)
 
-        # print(idea_submitted, skill_submitted)
-        # if(not idea_submitted and not skill_submitted):
-        #     dispatcher.utter_message(text = 'didnt quite get what the idea was...')
-        # else:
-        #     if(idea_submitted):
-                # dispatcher.utter_message(text = 'Taking 1 your idea of '+ str(tracker.get_slot('idea'))+' to the next level!')
-            # elif(skill_submitted):
-            #     dispatcher.utter_message(text = 'Taking your idea of '+ str(tracker.get_slot('skill'))+' to the next level!')
-        # res = df.loc[df['Idea'] == 'Papad Making', 'Skills'].to_list()
-        # res = res[0]
-        # print(res)
-        # print(tracker.get_slot('idea'))
-        # if (tracker.get_slot('idea') in ['cooking', 'cook', 'food']):
-        #     #dispatcher.utter_message(text="Here's somehting you need to know about cooking")
-        #     dispatcher.utter_message(text="Okay, the skills needed are {skills}".format(skills = res))
-        #     return [FollowupAction("action_tell_investment_returns")]
+        print(idea_submitted)
+        print(ideas)
+
+        if not idea_submitted:
+            dispatcher.utter_message(text = "Sorry, didnt get what you meant. Can you be more specific?")
+        else:
+            dispatcher.utter_message(text = 'Here\'s some more info about {idea}'.format(idea = idea_submitted[-1]))
+            dispatcher.utter_message(text = "Here's some general information...{info}".format(info = ideas[-1]['generalInfo']))
+            dispatcher.utter_message(text = "{idea} has these sub categories : {info}".format(idea =idea_submitted[-1], info = ", ".join(ideas[-1]["detailed_tags"][:-1])))
+
 
         return []
 
@@ -108,26 +119,23 @@ class ActionTellInvestment(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # res = df.loc[df['Idea'] == 'Papad Making', 'Investment':'Income'].to_dict()
-        # print(res)
-
         idea_submitted = tracker.get_slot('idea')
 
-        dispatcher.utter_message(text = 'Here\'s what you probably need to invest for' + str(idea_submitted) +' to work ')
-        dispatcher.utter_message(text = 'and here\'s what you probably will get in return for' + str(idea_submitted))
+        ideas = []
 
-        # skill_submitted = tracker.get_slot('skill')
+        for s in idea_submitted:
+            print(s)
+            for content in corpus:
+                if s in list(map(lambda x : x.lower(), content['detailed_tags'])):
+                    ideas.append(content)
 
-        # if not skill_submitted and not idea_submitted:
-        #     dispatcher.utter_message(text="Didn't quite catch that...")
+        print(ideas)
 
-        # else:
-        #     if(idea_submitted):
-        #         dispatcher.utter_message(text = 'Here\'s what you probably need to invest for' + str(tracker.get_slot('idea')) +' to work ')
-        #         dispatcher.utter_message(text = 'and here\'s what you probably will get in return for' + str(tracker.get_slot('idea')))
-        #     elif(skill_submitted):
-        #         dispatcher.utter_message(text = 'Here\'s what you probably need to invest for' + str(tracker.get_slot('skill')) +' to work ')
-        #         dispatcher.utter_message(text = 'and here\'s what you probably will get in return for' + str(tracker.get_slot('skill')))
+        if not idea_submitted:
+            dispatcher.utter_message(text = "Sorry, didnt get what you meant. Can you be more specific?")
+        else:
+            dispatcher.utter_message(text = 'You may need an investment in the range {min} to {max} INR.'.format(min = ideas[-1]['investment'][0], max = ideas[-1]['investment'][1] ))
+            dispatcher.utter_message(text = '{time}'.format(time = ideas[-1]['timeInvestment']))
 
         return []
 
@@ -144,22 +152,24 @@ class ActionSummarize(Action):
         x, y = tracker.get_slot('has_customers'), tracker.get_slot('has_competitors')
 
         idea_submitted = tracker.get_slot('idea')
+        
+        ideas = []
 
+        for s in idea_submitted[-1:]:
+            print(s)
+            for content in corpus:
+                if s in list(map(lambda x : x.lower(), content['detailed_tags'])):
+                    ideas.append(content)
 
-        dispatcher.utter_message(text="So to summarize, you are interested in"+ str(idea_submitted))
-        print(x, y)
-        # idea_submitted = tracker.get_slot('idea')
-        # skill_submitted = tracker.get_slot('skill')
+        if not idea_submitted:
+            dispatcher.utter_message(text = "Sorry, didnt get what you meant. Can you be more specific?")
+        else:
+            dispatcher.utter_message(text="So to summarize, we spoke about {ideas}".format(ideas = ', '.join(list(set(idea_submitted)))))
+            dispatcher.utter_message(text="You are interested in {idea}".format(idea = idea_submitted[-1]))
 
-        # if not skill_submitted and not idea_submitted:
-        #     dispatcher.utter_message(text="Didn't quite catch that...")
+            dispatcher.utter_message(text = 'You may need an investment in the range {min} to {max} INR.'.format(min = ideas[-1]['investment'][0], max = ideas[-1]['investment'][1] ))
+            dispatcher.utter_message(text = '{time}'.format(time = ideas[-1]['timeInvestment']))
 
-        # else:
-        #     if(idea_submitted):
-        #         dispatcher.utter_message(text = 'Here\'s what you probably need to invest for' + str(tracker.get_slot('idea')) +' to work ')
-        #         dispatcher.utter_message(text = 'and here\'s what you probably will get in return for' + str(tracker.get_slot('idea')))
-        #     elif(skill_submitted):
-        #         dispatcher.utter_message(text = 'Here\'s what you probably need to invest for' + str(tracker.get_slot('skill')) +' to work ')
-        #         dispatcher.utter_message(text = 'and here\'s what you probably will get in return for' + str(tracker.get_slot('skill')))
+            dispatcher.utter_message(text = "I hope I could help. Wishing you tons of luck!") 
 
         return []
